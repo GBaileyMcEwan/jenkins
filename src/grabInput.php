bba4f4cea6f0551708ccfb7c38a01b25f3379d6f
GBaileyMcEwan <39169795+GBaileyMcEwan@users.noreply.github.com>
2019-06-14 12:50:34 +0200

    Create grabInput.php

---
+++ src/grabInput.php
@@ -0,0 +1,17 @@
+<?php
+// Check if the form is submitted
+$myInput = $_POST['myInput'];
+// display the results
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '<title>F5 DevOps Day!</title>';
+echo '</head>';
+echo '<body style="background-color: #42F4EB;">';
+echo '<h1 style="font-family: Courier; color: #434347; font-size: 46px; text-align: center;">';
+echo 'Your input was: ' . $myInput . '';
+echo '</h1>';
+echo '</body>';
+echo '</html>';
+
+exit;
