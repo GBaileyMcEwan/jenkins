1b6b01aca551e4eaf8261c5b247903a0fd7a5ce9
GBaileyMcEwan <39169795+GBaileyMcEwan@users.noreply.github.com>
2019-06-14 14:00:09 +0200

    Update grabInput.php

--- src/grabInput.php
+++ src/grabInput.php
@@ -11,6 +11,7 @@ echo '<body style="background-color: #42F4EB;">';
 echo '<h1 style="font-family: Courier; color: #434347; font-size: 46px; text-align: center;">';
 echo 'Your input was: ' . $myInput . '';
 echo '</h1>';
+echo '<h2 style="font-family: Courier; color: #434347; font-size: 26px; text-align: center;"><a href="javascript:history.back();">[Go Back]</a></h2>';
 echo '</body>';
 echo '</html>';
 
